f795bf348862f8d4359e925aee2f83e4f66f3dc7
Aigar <aigarantson@gmail.com>
2013-11-04 20:58:23 +0200

    JFrame aken

--- Kodutöö 1 - simulaator/src/Main.java
+++ Kodutöö 1 - simulaator/src/Main.java
@@ -1,8 +1,19 @@
+import java.awt.*;
+
+import javax.swing.*;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		JFrame mainFrame = new JFrame("Simulaator");
+		mainFrame.setSize(700, 600);
+		
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		mainFrame.setLocation((screen.width - mainFrame.getWidth()) / 2, (screen.height - mainFrame.getHeight()) / 2);
+
+		mainFrame.setVisible(true);
 
 	}
 
