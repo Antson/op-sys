41c4f390e884d32fc71440424019ce694959d6dd
Aigar Antson <aigarantson@gmail.com>
2013-11-05 16:41:05 +0200

    bla

--- Kodutöö 1 - simulaator/src/Joonis.java
+++ Kodutöö 1 - simulaator/src/Joonis.java
@@ -1,37 +1,67 @@
 import java.awt.*;
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 public class Joonis extends JPanel {
 	
-	private int[] sisend = new int[5];
-	
-	public int[] getSisend() {
-		return sisend;
-	}
-
-	public void setSisend(int[] sisend) {
-		this.sisend = sisend;
-	}
+	private ArrayList<Integer> muster = new ArrayList<Integer>();
+	private String algoritm;
+	private Integer number = 0;
 
 	public void paintComponent(Graphics g) {
-		int x = 300;
-		int y = 40;
+		int x = 35;
+		int y = 60;
 		super.paintComponent(g);
 	    Graphics2D graphics2D = (Graphics2D) g;
 
-	    //Set  anti-alias!
 	    graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 	            RenderingHints.VALUE_ANTIALIAS_ON); 
+
+	    for (int i = 0; i < 32; i++, number++) {
+	    	graphics2D.drawString(number.toString(), 30+i*20, 45);
+	    }
+	    number = 0;
 	    
-	    for (int i = 0; i < 30; i++){
-	    	graphics2D.drawRect((i+1)*20, 10, 20, 20);
+	    for (int i = 0; i < muster.size() + 2; i++){
+	    	for (int j = 0; j < 32; j++){
+	    		graphics2D.drawRect(25+j*20, 30 + i*20, 20, 20);
+	    	}
 	    }
 	    
-		for (int arv: sisend){
-			graphics2D.drawLine(x, y, arv, y+20);
-			x = arv;
+	    int posX;
+		for (int i = 0; i < muster.size(); i++){
+			graphics2D.setColor(Color.black);
+			posX = 35 + 20*muster.get(i);
+			graphics2D.drawLine(x, y, posX, y+20);
+			x = posX;
 			y += 20;
+			graphics2D.fillOval(x-5, y-5, 10, 10);
+			try {
+				wait(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 		}
+
+	}
+	
+	public ArrayList getmuster() {
+		return muster;
+	}
+
+	public void setMuster(ArrayList muster) {
+		this.muster = muster;
+	}
+	
+	public String getAlgoritm() {
+		return algoritm;
+	}
+
+	public void setAlgoritm(String algoritm) {
+		this.algoritm = algoritm;
 	}
 
 }
--- Kodutöö 1 - simulaator/src/Main.java
+++ Kodutöö 1 - simulaator/src/Main.java
@@ -1,39 +1,86 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
 
-public class Main {
 
+public class Main {
+	
+	private static Joonis joonistus;
+	private static JFrame mainFrame;
+	
 	public static void main(String[] args) {
 		
-		JFrame mainFrame = new JFrame("Simulaator");
+		mainFrame = new JFrame("Simulaator");
 		mainFrame.setLayout(new GridLayout(2,1));
 		mainFrame.setSize(700, 600);
 		
-		Joonis joonistus = new Joonis();
+		joonistus = new Joonis();
 		joonistus.setBackground(Color.white);
-		
-		int[] sisend = {200, 400,500,300,500};
-		joonistus.setSisend(sisend);
 		mainFrame.add(joonistus);
+		
+		JPanel mainMenu = new JPanel();
+		mainMenu.setLayout(new GridBagLayout());
+		
+		JPanel mustrid = new JPanel(new GridLayout(3,1));
+		JButton näide1 = new JButton("Näide");
+		JButton näide2 = new JButton("Näide");
+		JButton näide3 = new JButton("Näide");
+		
+		näide1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setMuster("1 10 5 20");
+			}
+		});
+		
+		mustrid.add(näide1);
+		mustrid.add(näide2);
+		mustrid.add(näide3);
+		
+		mainMenu.add(mustrid, mustridConstraints());
+		
+		mainFrame.add(mainMenu);
+
 		
 		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 		mainFrame.setLocation((screen.width - mainFrame.getWidth()) / 2,
 				(screen.height - mainFrame.getHeight()) / 2);
-
+		mainFrame.setResizable(false);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setVisible(true);
 		
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	}
+	
+	private static GridBagConstraints mustridConstraints() {
+		GridBagConstraints gc = new GridBagConstraints();
+		gc.anchor = GridBagConstraints.NORTH;
+		gc.weightx = 0.2;
+		gc.weighty = 0d;
+		gc.gridwidth = GridBagConstraints.REMAINDER;
+		gc.fill = GridBagConstraints.NONE;
+		return gc;
+	}
+	
+	private GridBagConstraints nupudConstraints() {
+		GridBagConstraints gc = new GridBagConstraints();
+
+		gc.weightx = 0;
+		gc.anchor = GridBagConstraints.CENTER;
+		gc.gridwidth = GridBagConstraints.RELATIVE;
+
+		return gc;
+	}
+	
+	private static void setMuster(String s){
+		ArrayList<Integer> muster = new ArrayList<Integer>();
+		String[] arvud = s.split(" ");
+		for (String arv:arvud) {
+			muster.add(Integer.parseInt(arv));
 		}
-		
-		int[] sisend2 = {200, 100,500,400,250,300};
-		joonistus.setSisend(sisend2);
-		joonistus.paint(joonistus.getGraphics());
+		joonistus.setMuster(muster);
 
 	}
 
