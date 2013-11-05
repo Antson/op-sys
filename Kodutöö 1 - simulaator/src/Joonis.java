5708257c405917444d660c005f59b32e86e9b499
Aigar <aigarantson@gmail.com>
2013-11-05 20:56:16 +0200

    3 algoritmi tehtud

--- Kodutöö 1 - simulaator/src/Joonis.java
+++ Kodutöö 1 - simulaator/src/Joonis.java
@@ -1,53 +1,113 @@
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import javax.swing.*;
 
 public class Joonis extends JPanel {
-	
+
 	private ArrayList<Integer> muster = new ArrayList<Integer>();
-	private String algoritm;
+	private String algoritm = "FCFS";
 	private Integer number = 0;
 
 	public void paintComponent(Graphics g) {
-		int x = 35;
+		int x = 235;
 		int y = 60;
+		int posX;
 		super.paintComponent(g);
-	    Graphics2D graphics2D = (Graphics2D) g;
+		Graphics2D graphics2D = (Graphics2D) g;
 
-	    graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-	            RenderingHints.VALUE_ANTIALIAS_ON); 
+		graphics2D.setColor(Color.black);
+		graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		Font def =  g.getFont();
+		
+		
+		g.setFont(new Font("", Font.BOLD, 18));
+		
+		graphics2D.drawString("Algoritm:", 25, 25);
+		graphics2D.drawString(algoritm, 120, 25);
+		graphics2D.drawString("Pöördusmuster:", 220, 25);
+		graphics2D.drawString(muster.toString(), 365,25);
+		
+		
+		g.setFont(def);
+		
+		for (int i = 0; i < 32; i++, number++) {
+			graphics2D.drawString(number.toString(), 30 + i * 20, 45);
+		}
+		number = 0;
 
-	    for (int i = 0; i < 32; i++, number++) {
-	    	graphics2D.drawString(number.toString(), 30+i*20, 45);
-	    }
-	    number = 0;
-	    
-	    for (int i = 0; i < muster.size() + 2; i++){
-	    	for (int j = 0; j < 32; j++){
-	    		graphics2D.drawRect(25+j*20, 30 + i*20, 20, 20);
-	    	}
-	    }
-	    
-	    int posX;
-		for (int i = 0; i < muster.size(); i++){
-			graphics2D.setColor(Color.black);
-			posX = 35 + 20*muster.get(i);
-			graphics2D.drawLine(x, y, posX, y+20);
-			x = posX;
-			y += 20;
-			graphics2D.fillOval(x-5, y-5, 10, 10);
-			try {
-				wait(2000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		for (int i = 0; i < muster.size() + 2; i++) {
+			for (int j = 0; j < 32; j++) {
+				graphics2D.drawRect(25 + j * 20, 30 + i * 20, 20, 20);
 			}
-
+		}
+		
+		int algus = 10;
+		graphics2D.fillOval(x - 5, y - 5, 10, 10);
+		if (algoritm == "FCFS") {
+			for (int i = 0; i < muster.size(); i++) {
+				posX = 35 + 20 * muster.get(i);
+				graphics2D.drawLine(x, y, posX, y + 20);
+				x = posX;
+				y += 20;
+				graphics2D.fillOval(x - 5, y - 5, 10, 10);
+			}
+		} else if (algoritm == "SSTF") {
+			ArrayList<Integer> koopia = new ArrayList(muster);
+			while (koopia.size() > 0){
+				int min = 31;
+				int lühim = algus;
+				for (int i = 0; i < koopia.size(); i++) {
+					if (Math.abs(koopia.get(i) - algus) < min) {
+						min = Math.abs(koopia.get(i) - algus);
+						lühim = i;
+					}
+				}
+				posX = 35 + 20 * koopia.get(lühim);
+				graphics2D.drawLine(x, y, posX, y + 20);
+				x = posX;
+				y += 20;
+				graphics2D.fillOval(x - 5, y - 5, 10, 10);
+				algus = koopia.get(lühim);
+				koopia.remove(lühim);
+			}
+		} else if (algoritm == "SCAN") {
+			ArrayList<Integer> koopia = new ArrayList(muster);
+			Collections.sort(koopia);
+			int max = Collections.max(koopia);
+			int päringuid = koopia.size();
+			for (int i = 0;;){
+				if (koopia.get(i) == max) {
+					break;
+				}
+				if (koopia.get(i) >= algus) {
+					posX = 35 + 20 * koopia.get(i);
+					graphics2D.drawLine(x, y, posX, y + 20);
+					x = posX;
+					y += 20;
+					graphics2D.fillOval(x - 5, y - 5, 10, 10);
+					algus = koopia.get(i);
+					koopia.remove(i);
+				} else {
+					i++;
+				}
+			}
+			if (koopia.size() > 0) {
+				for (int i = koopia.size(); i > 0; i--) {
+					posX = 35 + 20 * koopia.get(i-1);
+					graphics2D.drawLine(x, y, posX, y + 20);
+					x = posX;
+					y += 20;
+					graphics2D.fillOval(x - 5, y - 5, 10, 10);
+				}
+			}
 		}
 
 	}
-	
+
 	public ArrayList getmuster() {
 		return muster;
 	}
@@ -55,7 +115,7 @@ public class Joonis extends JPanel {
 	public void setMuster(ArrayList muster) {
 		this.muster = muster;
 	}
-	
+
 	public String getAlgoritm() {
 		return algoritm;
 	}
--- Kodutöö 1 - simulaator/src/Main.java
+++ Kodutöö 1 - simulaator/src/Main.java
@@ -5,45 +5,128 @@ import java.util.ArrayList;
 
 import javax.swing.*;
 
-
-
 public class Main {
-	
+
 	private static Joonis joonistus;
 	private static JFrame mainFrame;
-	
+	private static JTextField omaMuster;
+
 	public static void main(String[] args) {
-		
+
 		mainFrame = new JFrame("Simulaator");
-		mainFrame.setLayout(new GridLayout(2,1));
+		mainFrame.setLayout(new GridLayout(2, 1));
 		mainFrame.setSize(700, 600);
-		
+
 		joonistus = new Joonis();
 		joonistus.setBackground(Color.white);
 		mainFrame.add(joonistus);
-		
+
 		JPanel mainMenu = new JPanel();
-		mainMenu.setLayout(new GridBagLayout());
-		
-		JPanel mustrid = new JPanel(new GridLayout(3,1));
-		JButton näide1 = new JButton("Näide");
-		JButton näide2 = new JButton("Näide");
-		JButton näide3 = new JButton("Näide");
-		
+		mainMenu.setLayout(new GridLayout(3, 1));
+
+		JPanel mustrid = new JPanel(new FlowLayout());
+		JButton näide1 = new JButton("1 10 5 20");
+		JButton näide2 = new JButton("18 30 15 0 23 7 16");
+		JButton näide3 = new JButton("8 24 11 2 19 13 32 15 4 12");
+
 		näide1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setMuster("1 10 5 20");
 			}
 		});
-		
+
+		näide2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setMuster("18 30 15 0 23 7 16");
+			}
+		});
+
+		näide3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setMuster("8 24 11 2 19 13 31 15 4 12");
+			}
+		});
+
 		mustrid.add(näide1);
 		mustrid.add(näide2);
 		mustrid.add(näide3);
-		
-		mainMenu.add(mustrid, mustridConstraints());
-		
-		mainFrame.add(mainMenu);
 
+		mainMenu.add(mustrid);
+
+		omaMuster = new JTextField();
+		JButton simuleeriNupp = new JButton("Simuleeri oma \n"
+				+ "päringute järjekord");
+		JPanel omaSisend = new JPanel();
+		omaSisend.setLayout(new FlowLayout());
+		omaSisend.add(simuleeriNupp, BorderLayout.WEST);
+		omaMuster.setColumns(20);
+		omaSisend.add(omaMuster, BorderLayout.CENTER);
+
+
+		simuleeriNupp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String s = omaMuster.getText();
+				if (!(s.equals(""))) {
+					String[] arvud = s.split(" ");
+					if (arvud.length > 10) {
+						JOptionPane.showMessageDialog(null,
+								"Sisend liiga pikk.\n Maksimaalselt 10 täisarvu vahemikus 0 kuni 31.", 
+								"Vale sisend", JOptionPane.WARNING_MESSAGE);
+					} else {
+						try {
+							for (String sArv: arvud) {
+								int arv = Integer.parseInt(sArv);
+								if (arv > 31 || arv < 0) {
+									throw new IndexOutOfBoundsException();
+								}
+							}
+							setMuster(s);
+						} catch (NumberFormatException ex) {
+							JOptionPane.showMessageDialog(null,
+									"Ei sisestatud täisarve või leidub üleliigseid tühikuid."
+									+ "\nNumbrite vahel peab olema üks tühik.",
+									"Vale sisend", JOptionPane.WARNING_MESSAGE);
+						} catch (IndexOutOfBoundsException ex) {
+							JOptionPane.showMessageDialog(null,
+									"Sisestatud täisarvud peavad olema vahemikus 0 kuni 31.", "Vale sisend",
+									JOptionPane.WARNING_MESSAGE);
+						}
+					}
+					
+				}
+			}
+		});
+		mainMenu.add(omaSisend);
+		mainFrame.add(mainMenu);
+		
+		//Algoritmide nupud
+		JPanel algoritmid = new JPanel(new FlowLayout());
+		
+		JButton alg1 = new JButton("FCFS");
+		JButton alg2 = new JButton("SSTF");
+		JButton alg3 = new JButton("SCAN");
+		JButton alg4 = new JButton("LOOK");
+		JButton alg5 = new JButton("C-SCAN");
+		JButton alg6 = new JButton("C-LOOK");
+		JButton alg7 = new JButton("NOOP");
+		
+		algoritmid.add(alg1);
+		algoritmid.add(alg2);
+		algoritmid.add(alg3);
+		algoritmid.add(alg4);
+		algoritmid.add(alg5);
+		algoritmid.add(alg6);
+		algoritmid.add(alg7);
+		
+		alg1.addActionListener(new Nupukuular());
+		alg2.addActionListener(new Nupukuular());
+		alg3.addActionListener(new Nupukuular());
+		alg4.addActionListener(new Nupukuular());
+		alg5.addActionListener(new Nupukuular());
+		alg6.addActionListener(new Nupukuular());
+		alg7.addActionListener(new Nupukuular());
+		
+		mainMenu.add(algoritmid);
 		
 		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 		mainFrame.setLocation((screen.width - mainFrame.getWidth()) / 2,
@@ -51,19 +134,19 @@ public class Main {
 		mainFrame.setResizable(false);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setVisible(true);
-		
+
 	}
-	
+
 	private static GridBagConstraints mustridConstraints() {
 		GridBagConstraints gc = new GridBagConstraints();
-		gc.anchor = GridBagConstraints.NORTH;
-		gc.weightx = 0.2;
-		gc.weighty = 0d;
-		gc.gridwidth = GridBagConstraints.REMAINDER;
-		gc.fill = GridBagConstraints.NONE;
+		gc.anchor = GridBagConstraints.SOUTH;
+		// gc.weightx = 0.2;
+		// gc.weighty = 0d;
+		gc.gridwidth = GridBagConstraints.RELATIVE;
+		gc.fill = GridBagConstraints.VERTICAL;
 		return gc;
 	}
-	
+
 	private GridBagConstraints nupudConstraints() {
 		GridBagConstraints gc = new GridBagConstraints();
 
@@ -73,15 +156,28 @@ public class Main {
 
 		return gc;
 	}
-	
-	private static void setMuster(String s){
+
+	private static void setMuster(String s) {
 		ArrayList<Integer> muster = new ArrayList<Integer>();
 		String[] arvud = s.split(" ");
-		for (String arv:arvud) {
+		for (String arv : arvud) {
 			muster.add(Integer.parseInt(arv));
 		}
 		joonistus.setMuster(muster);
-
+		joonistus.repaint();
+	}
+	
+	public static Joonis getJoonistus() {
+		return joonistus;
 	}
 
+
+}
+
+class Nupukuular implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+		Main.getJoonistus().setAlgoritm(((JButton)e.getSource()).getText());
+		Main.getJoonistus().repaint();
+	}
+	
 }
